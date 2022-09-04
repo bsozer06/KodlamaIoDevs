c90eac184739d1b2ac453ca633b1291773233103
burhan sozer <you@example.com>
2022-09-04 16:17:32 +0300

    Developed the deleting for the programming language

--- src/kodlamaIoDevs/KodlamaIoDevs.Application/Features/ProgrammingLanguage/Profiles/MappingProfiles.cs
+++ src/kodlamaIoDevs/KodlamaIoDevs.Application/Features/ProgrammingLanguage/Profiles/MappingProfiles.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Core.Persistence.Paging;
 using KodlamaIoDevs.Application.Features.ProgrammingLanguage.Commands.CreateProgramingLanguage;
+using KodlamaIoDevs.Application.Features.ProgrammingLanguage.Commands.DeleteProgramingLanguage;
 using KodlamaIoDevs.Application.Features.ProgrammingLanguage.Commands.UpdateProgramingLanguage;
 using KodlamaIoDevs.Application.Features.ProgrammingLanguage.Dtos;
 using KodlamaIoDevs.Application.Features.ProgrammingLanguage.Models;
@@ -16,15 +17,23 @@ namespace KodlamaIoDevs.Application.Features.ProgrammingLanguage.Profiles
     {
         public MappingProfiles()
         {
+            // creating
             CreateMap<Domain.Entities.ProgrammingLanguage, CreatedProgrammingLanguageDto>().ReverseMap();
             CreateMap<Domain.Entities.ProgrammingLanguage, CreateProgrammingLanguageCommand>().ReverseMap();
 
+            // updating
             CreateMap<Domain.Entities.ProgrammingLanguage, UpdatedProgrammingLanguageDto>().ReverseMap();
             CreateMap<Domain.Entities.ProgrammingLanguage, UpdateProgrammingLanguageCommand>().ReverseMap();
 
+            // deleting
+            CreateMap<Domain.Entities.ProgrammingLanguage, DeletedProgrammingLanguageDto>().ReverseMap();
+            CreateMap<Domain.Entities.ProgrammingLanguage, DeleteProgrammingLanguageCommand>().ReverseMap();
+
+            // get list
             CreateMap<IPaginate<Domain.Entities.ProgrammingLanguage>, ProgrammingLanguageListModel>();
             CreateMap<Domain.Entities.ProgrammingLanguage, ProgrammingLanguageDto>().ReverseMap();
 
+            // get by id
             CreateMap<Domain.Entities.ProgrammingLanguage, ProgrammingLanguageGetByIdDto>().ReverseMap();
 
         }
--- src/kodlamaIoDevs/KodlamaIoDevs.WebAPI/Controllers/ProgrammingLanguagesController.cs
+++ src/kodlamaIoDevs/KodlamaIoDevs.WebAPI/Controllers/ProgrammingLanguagesController.cs
@@ -1,9 +1,9 @@
 ﻿using Core.Application.Requests;
 using KodlamaIoDevs.Application.Features.ProgrammingLanguage.Commands.CreateProgramingLanguage;
+using KodlamaIoDevs.Application.Features.ProgrammingLanguage.Commands.DeleteProgramingLanguage;
 using KodlamaIoDevs.Application.Features.ProgrammingLanguage.Commands.UpdateProgramingLanguage;
 using KodlamaIoDevs.Application.Features.ProgrammingLanguage.Queries.GetByIdProgrammingLanguage;
 using KodlamaIoDevs.Application.Features.ProgrammingLanguage.Queries.GetListProgrammingLanguage;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KodlamaIoDevs.WebAPI.Controllers
@@ -12,34 +12,41 @@ namespace KodlamaIoDevs.WebAPI.Controllers
     [ApiController]
     public class ProgrammingLanguagesController : BaseController
     {
-        [HttpGet]
+        [HttpGet(nameof(GetList))]
         public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
         {
             var getProgramLangListQuery = new GetListProgrammingLanguageQuery() { PageRequest = pageRequest };
-            var result = await Mediator.Send(getProgramLangListQuery);
+            var result = await Mediator!.Send(getProgramLangListQuery);
             return Ok(result);
         }
 
 
-        [HttpGet("{Id}")]
+        [HttpGet("GetById/{Id}")]
         public async Task<IActionResult> GetById([FromRoute] GetByIdProgrammingLanguageQuery getByIdProgrammingLanguageQuery)
         {
-            var result = await Mediator.Send(getByIdProgrammingLanguageQuery);
+            var result = await Mediator!.Send(getByIdProgrammingLanguageQuery);
             return Ok(result);
         }
 
 
-        [HttpPost]
+        [HttpPost(nameof(Add))]
         public async Task<IActionResult> Add([FromBody] CreateProgrammingLanguageCommand createProgrammingLanguageCommand)
         {
-            var result = await Mediator.Send(createProgrammingLanguageCommand);
+            var result = await Mediator!.Send(createProgrammingLanguageCommand);
             return Created("", result);
         }
 
-        [HttpPut]
+        [HttpPut(nameof(Update))]
         public async Task<IActionResult> Update([FromBody] UpdateProgrammingLanguageCommand updateProgrammingLanguageCommand)
         {
-            var result = await Mediator.Send(updateProgrammingLanguageCommand);
+            var result = await Mediator!.Send(updateProgrammingLanguageCommand);
+            return NoContent();
+        }
+
+        [HttpDelete("Delete/{Id}")]
+        public async Task<IActionResult> Delete([FromRoute] DeleteProgrammingLanguageCommand deleteProgrammingLanguageCommand)
+        {
+            var result = await Mediator!.Send(deleteProgrammingLanguageCommand);
             return NoContent();
         }
 
