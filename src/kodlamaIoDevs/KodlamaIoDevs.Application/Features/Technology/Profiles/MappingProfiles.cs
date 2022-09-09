75a1b4e9b76530729fa4d0c7d6cc963e92385685
burhan sozer <you@example.com>
2022-09-10 00:05:32 +0300

    Developed GetListTechnology

---
+++ src/kodlamaIoDevs/KodlamaIoDevs.Application/Features/Technology/Profiles/MappingProfiles.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Core.Persistence.Paging;
+using KodlamaIoDevs.Application.Features.Technology.Dtos;
+using KodlamaIoDevs.Application.Features.Technology.Models;
+
+namespace KodlamaIoDevs.Application.Features.Technology.Profiles
+{
+    public class MappingProfiles: Profile
+    {
+        public MappingProfiles()
+        {
+
+            // Query
+            CreateMap<IPaginate<Domain.Entities.Technology>, TechnologyListModel>().ReverseMap();
+            CreateMap<Domain.Entities.Technology, TechnologyListDto>()
+                .ForMember(target => target.ProgrammingLanguageName, opt =>
+                    opt.MapFrom(resource => resource.ProgrammingLanguage.Name)).ReverseMap();
+
+        }
+    }
+}
---
+++ src/kodlamaIoDevs/KodlamaIoDevs.WebAPI/Controllers/TechnologiesController.cs
@@ -0,0 +1,17 @@
+﻿using Core.Application.Requests;
+using KodlamaIoDevs.Application.Features.Technology.Queries.GetListTecnology;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KodlamaIoDevs.WebAPI.Controllers
+{
+    public class TechnologiesController: BaseController
+    {
+        [HttpGet(nameof(GetList))]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            var getTechnologyListQuery = new GetListTechnologyQuery() { PageRequest = pageRequest };
+            var result = await Mediator!.Send(getTechnologyListQuery);
+            return Ok(result);
+        }
+    }
+}
