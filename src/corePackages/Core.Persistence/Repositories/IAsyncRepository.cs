18e305cc10d69845f886c059b6879b9c0704152b
burhan sozer <you@example.com>
2022-09-10 21:33:16 +0300

    Adedd new method in Core async repository

--- src/corePackages/Core.Persistence/Repositories/IAsyncRepository.cs
+++ src/corePackages/Core.Persistence/Repositories/IAsyncRepository.cs
@@ -8,11 +8,15 @@ public interface IAsyncRepository<T> : IQuery<T> where T : Entity
 {
     Task<T?> GetAsync(Expression<Func<T, bool>> predicate);
 
+    Task<T> GetAsync(Expression<Func<T, bool>> predicate,
+                                       Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null);
+
+
     Task<IPaginate<T>> GetListAsync(Expression<Func<T, bool>>? predicate = null,
-                                    Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
-                                    Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
-                                    int index = 0, int size = 10, bool enableTracking = true,
-                                    CancellationToken cancellationToken = default);
+                                Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+                                int index = 0, int size = 10, bool enableTracking = true,
+                                CancellationToken cancellationToken = default);
 
     Task<IPaginate<T>> GetListByDynamicAsync(Dynamic.Dynamic dynamic,
                                              Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
